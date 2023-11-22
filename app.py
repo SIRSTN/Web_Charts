05c5d304200419bb6e7f0d13a45cf1783ccc76a8
SIRSTN <stephane.velen@gmail.com>
2023-11-22 17:14:35 +0100

    Filter Date commit

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
+from datetime import datetime, timedelta
 import pandas as pd
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
-import plotly.io as pio 
+import plotly.io as pio
+import pytz
 
 app = Flask(__name__)
 
-def create_chart(source):
+def create_chart(source, from_date=None, to_date=None):
     # Connect to MongoDB
     client = MongoClient('mongodb://localhost:27017')
     db = client.SocialMedia_Analysis
     collection = db.Sentiment_Averages
 
-    # Query the database for the given source
+    # Prepare query with date filters
     query = {"source": source}
+    if from_date and to_date:
+        from_date_utc = datetime.strptime(from_date, '%Y-%m-%d').replace(tzinfo=pytz.UTC)
+        to_date_utc = datetime.strptime(to_date, '%Y-%m-%d').replace(tzinfo=pytz.UTC)
+        query['timestamp'] = {'$gte': from_date_utc, '$lte': to_date_utc}
+
     documents = collection.find(query)
 
-    # Create a DataFrame
+    # Create a DataFrame and handle empty DataFrame
     df = pd.DataFrame(list(documents))
+    if df.empty:
+        return '<p>No data available for the selected date range.</p>'
 
-    # Ensure timestamp is a datetime object for proper plotting
+    # Convert 'timestamp' to datetime
     df['timestamp'] = pd.to_datetime(df['timestamp'])
 
     # Create a subplot with two y-axes
@@ -54,15 +63,28 @@ def create_chart(source):
     # Convert plot to HTML string
     return pio.to_html(fig, full_html=False)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def charts():
-    # Create charts for Mastodon and Reddit
-    graph_html_mastodon = create_chart("Mastodon")
-    graph_html_reddit = create_chart("Reddit")
+    # Default 'to_date' is tomorrow at the end of the day
+    default_to_date = datetime.now() + timedelta(days=1)
+    default_from_date = default_to_date - timedelta(days=30)
 
-    return render_template('chart.html',
-                           graph_html_mastodon=graph_html_mastodon,
-                           graph_html_reddit=graph_html_reddit)
+    # Format the dates as strings
+    default_from_date_str = default_from_date.strftime('%Y-%m-%d')
+    default_to_date_str = default_to_date.strftime('%Y-%m-%d')
+
+    # Get the dates from the request, or use the defaults
+    from_date = request.args.get('from_date', default_from_date_str)
+    to_date = request.args.get('to_date', default_to_date_str)
+
+    graph_html_mastodon = create_chart("Mastodon", from_date, to_date)
+    graph_html_reddit = create_chart("Reddit", from_date, to_date)
+    
+    return render_template('chart.html', 
+                           graph_html_mastodon=graph_html_mastodon, 
+                           graph_html_reddit=graph_html_reddit,
+                           from_date=from_date, 
+                           to_date=to_date)
 
 if __name__ == '__main__':
     app.run(debug=True)
