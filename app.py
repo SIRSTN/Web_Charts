69946c40d9031eb9a31c61d44ea87b64bec70cd5
SIRSTN <stephane.velen@gmail.com>
2023-11-27 12:42:45 +0100

    average charts commit

--- app.py
+++ app.py
@@ -15,13 +15,10 @@ config = ConfigParser()
 config.read('config.ini')
 
 def create_chart(source, from_date=None, to_date=None, keyword='Bitcoin'):
-
-    # Connect to MongoDB
     client = MongoClient(config.get('Web_Charts', 'MongoClient'))
     db = client.Cluster0
     collection = db.Sentiment_Averages
 
-    # Prepare query with date filters and keyword
     query = {"source": source, "keyword": keyword}
     if from_date and to_date:
         from_date_utc = datetime.strptime(from_date, '%Y-%m-%d').replace(tzinfo=pytz.UTC)
@@ -30,66 +27,80 @@ def create_chart(source, from_date=None, to_date=None, keyword='Bitcoin'):
 
     documents = collection.find(query)
 
-    # Create a DataFrame and handle empty DataFrame
     df = pd.DataFrame(list(documents))
     if df.empty:
         return '<p>No data available for the selected date range.</p>'
 
-    # Convert 'timestamp' to datetime
     df['timestamp'] = pd.to_datetime(df['timestamp'])
 
-    # Create a subplot with two y-axes
     fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(go.Scatter(x=df['timestamp'], y=df['price'], name='Price'), secondary_y=False)
+    fig.add_trace(go.Scatter(x=df['timestamp'], y=df['textblob_sentiment'], name='TextBlob Sentiment'), secondary_y=True)
+    fig.add_trace(go.Scatter(x=df['timestamp'], y=df['vader_sentiment'], name='VADER Sentiment'), secondary_y=True)
 
-    # Add Bitcoin price plot
-    fig.add_trace(
-        go.Scatter(x=df['timestamp'], y=df['price'], name='Price'),
-        secondary_y=False,
-    )
-
-    # Add TextBlob sentiment plot
-    fig.add_trace(
-        go.Scatter(x=df['timestamp'], y=df['textblob_sentiment'], name='TextBlob Sentiment'),
-        secondary_y=True,
-    )
-
-    # Add VADER sentiment plot on the same axis as TextBlob
-    fig.add_trace(
-        go.Scatter(x=df['timestamp'], y=df['vader_sentiment'], name='VADER Sentiment'),
-        secondary_y=True,
-    )
-
-    # Set x-axis title
     fig.update_xaxes(title_text="Timestamp")
-
-    # Set y-axes titles
     fig.update_yaxes(title_text="<b>Primary</b> Price", secondary_y=False)
     fig.update_yaxes(title_text="<b>Secondary</b> Sentiment Score", secondary_y=True)
 
-    # Convert plot to HTML string
+    return pio.to_html(fig, full_html=False)
+
+def create_chart_all_sources(from_date=None, to_date=None, keyword='Bitcoin'):
+    client = MongoClient(config.get('Web_Charts', 'MongoClient'))
+    db = client.Cluster0
+    collection = db.Sentiment_Averages
+
+    query = {"keyword": keyword}
+    if from_date and to_date:
+        from_date_utc = datetime.strptime(from_date, '%Y-%m-%d').replace(tzinfo=pytz.UTC)
+        to_date_utc = datetime.strptime(to_date, '%Y-%m-%d').replace(tzinfo=pytz.UTC)
+        query['timestamp'] = {'$gte': from_date_utc, '$lte': to_date_utc}
+
+    pipeline = [
+        {"$match": query},
+        {"$group": {
+            "_id": {"$dateToString": {"format": "%Y-%m-%d", "date": "$timestamp"}},
+            "avg_price": {"$avg": "$price"},
+            "avg_textblob_sentiment": {"$avg": "$textblob_sentiment"},
+            "avg_vader_sentiment": {"$avg": "$vader_sentiment"}
+        }},
+        {"$sort": {"_id": 1}}
+    ]
+    aggregated_data = collection.aggregate(pipeline)
+
+    df = pd.DataFrame(list(aggregated_data))
+
+    if df.empty:
+        return '<p>No data available for the selected date range.</p>'
+
+    df['_id'] = pd.to_datetime(df['_id'])
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(go.Scatter(x=df['_id'], y=df['avg_price'], name='Average Price'), secondary_y=False)
+    fig.add_trace(go.Scatter(x=df['_id'], y=df['avg_textblob_sentiment'], name='Average TextBlob Sentiment'), secondary_y=True)
+    fig.add_trace(go.Scatter(x=df['_id'], y=df['avg_vader_sentiment'], name='Average VADER Sentiment'), secondary_y=True)
+
     return pio.to_html(fig, full_html=False)
 
 @app.route('/', methods=['GET'])
 def charts():
-    # Default 'to_date' is tomorrow at the end of the day
     default_to_date = datetime.now() + timedelta(days=1)
     default_from_date = default_to_date - timedelta(days=30)
 
-    # Format the dates as strings
     default_from_date_str = default_from_date.strftime('%Y-%m-%d')
     default_to_date_str = default_to_date.strftime('%Y-%m-%d')
 
-    # Get the dates and keyword from the request, or use the defaults
     from_date = request.args.get('from_date', default_from_date_str)
     to_date = request.args.get('to_date', default_to_date_str)
     keyword = request.args.get('keyword', 'Bitcoin')
 
     graph_html_mastodon = create_chart("Mastodon", from_date, to_date, keyword)
     graph_html_reddit = create_chart("Reddit", from_date, to_date, keyword)
-    
+    graph_html_all_sources = create_chart_all_sources(from_date, to_date, keyword)
+
     return render_template('chart.html', 
                            graph_html_mastodon=graph_html_mastodon, 
                            graph_html_reddit=graph_html_reddit,
+                           graph_html_all_sources=graph_html_all_sources,
                            from_date=from_date, 
                            to_date=to_date,
                            keyword=keyword)
